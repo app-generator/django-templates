0e2f361760049d86c19f7c097ca83c7fb1453acb
App Generator <support@appseed.us>
2021-06-08 16:23:31 +0300

    Added sample app

---
+++ sample_app/views.py
@@ -0,0 +1,33 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from django.shortcuts import render
+from django.template import loader
+
+from django.http import HttpResponse
+from django import template
+
+# Create your views here.
+
+def pages(request):
+
+    context        = {}
+    requested_html = request.path.split('/')[-1]
+    
+    if not requested_html or requested_html == '':
+        requested_html = 'index.html'
+
+    try:
+
+        html_file = loader.get_template( requested_html )
+        return HttpResponse(html_file.render(context, request))    
+
+    except template.TemplateDoesNotExist:
+
+        return HttpResponse("template not found = " + requested_html, status=404)
+
+    except:
+
+        return HttpResponse("Error 500" , status=500)
